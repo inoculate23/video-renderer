1a6ef528044d0b1ca563e98aefb8b3341537f775
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2025-06-27 05:47:13 -0700

    Add files via upload

---
+++ lib/demo/util/renderDemo.chrome.d.ts
@@ -0,0 +1,2 @@
+import { ComplexFilter } from '../../browser';
+export declare const main: (assets: string[], complexFilters: ComplexFilter[]) => Promise<void>;
---
+++ lib/demo/util/renderDemo.d.ts
@@ -0,0 +1,3 @@
+import { ComplexFilter } from '../../browser';
+declare const renderDemo: (assets: string[], complexFilters: ComplexFilter[]) => any;
+export default renderDemo;
